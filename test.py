1efa7a3b0d283efe3db22e42d34a28585cb58114
colonoh <steve.j.warner@gmail.com>
2015-08-16 10:58:51 -0400

    No preview test

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import cv2
+import picamera
 import numpy as np
+import time
 
-img = cv2.imread('dark.jpg')
-px = img[100,100]
-print(px)
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    #camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('foo.jpg')
