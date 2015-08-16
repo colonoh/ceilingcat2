89e87c80ec25875122c018201303ea6f606ede18
colonoh <steve.j.warner@gmail.com>
2015-08-16 11:53:53 -0400

    Saving color and greyscale images

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import cv2
 import picamera
+import picamera.array
 import numpy as np
 import time
 
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
     #camera.start_preview()
-    # Camera warm-up time
     time.sleep(2)
-    camera.capture('foo.jpg')
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.capture(stream, format='bgr', use_video_port=True)
+        # At this point the image is available as stream.array
+        image = stream.array
+        stream.truncate(0)
+
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('test.jpg', gray)
+        cv2.imwrite('color.jpg', image)
