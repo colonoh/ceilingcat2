d11a2ca68edb026536d5033fe676afe5617041ce
colonoh <steve.j.warner@gmail.com>
2015-08-23 12:28:23 -0400

    added counter to determine whether recording should start or stop

--- custom_video_start.py
+++ custom_video_start.py
@@ -3,26 +3,19 @@ import picamera.array
 import numpy as np
 import datetime, time
 
-outputPath = '/dev/null'
+motionDetected = 0
 
 class MyMotionDetector(picamera.array.PiMotionAnalysis):
     def analyse(self, a):
-        timestamp1 = time.time()
-        a = np.sqrt(
-            np.square(a['x'].astype(np.float)) +
-            np.square(a['y'].astype(np.float))
-            ).clip(0, 255).astype(np.uint8)
-        # If there're more than 10 vectors with a magnitude greater
-        # than 60, then say we've detected motion
-        global outputPath
-        #print((a>10).sum())
-        if (a > 10).sum() > 10:
-            #print('Motion detected!')
-            outputPath = 'data.h264'
+        global motionDetected
+        a = np.sqrt(np.square(a['x'].astype(np.float)) + np.square(a['y'].astype(np.float))).clip(0, 255).astype(np.uint8)
+        # If there're more than 10 vectors with a magnitude greater than 50, then say we've detected motion
+        if (a > 20).sum() > 20:
+            if motionDetected < 6.:
+                motionDetected += 1.
         else:
-            outputPath = '/dev/null'
-        timestamp2 = time.time()
-        #print(timestamp2-timestamp1)
+            if motionDetected > 0.:
+                motionDetected -= .2
 
 startTime = time.time() # time that camera turned on
 with picamera.PiCamera() as camera:
@@ -31,21 +24,15 @@ with picamera.PiCamera() as camera:
     time.sleep(2)
     camera.iso = 800
     camera.start_recording('/dev/null', format='h264', motion_output=MyMotionDetector(camera))
-    recordingMotion = False # currently recording or not
 
     print('Started!')
     while time.time() - startTime < 20:
-        timestamp = datetime.datetime.now()
+        if motionDetected > 3.: # if there is motion
+            camera.start_recording('{}.h264'.format(datetime.datetime.now()), splitter_port=2, intra_period=3)
 
-        # if there is motion
-        if outputPath == 'data.h264' and time.time() - startTime > 3:
-            #camera.split_recording('{}.h264'.format(timestamp), format='h264', motion_output=MyMotionDetector(camera))
-            camera.start_recording('{}.h264'.format(timestamp), splitter_port=2, intra_period=3)
-
-            # while there is motion
-            while(outputPath == 'data.h264'):
+            while motionDetected > 0.: # while there is motion
                 camera.annotate_text = str(datetime.datetime.now())
-                camera.wait_recording(1,splitter_port=2)
+                camera.wait_recording(.1,splitter_port=2)
 
             # no more motion
             camera.stop_recording(splitter_port=2)
